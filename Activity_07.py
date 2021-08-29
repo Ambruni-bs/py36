9fb5b5b1e80bd217a49ac57517eceec0de4cbe65
Ambruni-bs <89661010+Ambruni-bs@users.noreply.github.com>
2021-08-29 09:41:48 +0530

    Update Activity_07.py

--- Activity_07.py
+++ Activity_07.py
@@ -2,3 +2,11 @@ a = int(input("Enter the first number"))
 b = int(input("Enter the second number"))
 c = a + b
 print(str(a) + ' + ' + str(b) + ' = ' + str(c))
+
+#other ways to do formatting before printing
+
+print(a,end=' + ')
+print(b,end=' = ')
+print(c)
+
+print( '{x}  +  {y}  = {z}' .format(x=a,y=b,z=c))
