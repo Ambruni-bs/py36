715eace46a0820b6276255df953818676a987a60
Ambruni-bs <89661010+Ambruni-bs@users.noreply.github.com>
2021-08-29 11:40:54 +0530

    Update Activity_09.py

--- Activity_09.py
+++ Activity_09.py
@@ -6,6 +6,6 @@ import math
 v = ((b*b)*(h*h)) / math.sqrt(k)
 print("volume of the tromboloid is ")
 print('%0.3f'%v)
-r = ((3/4)*v*(22/7))**(1/3)
+r = ((3/4)*v*(math.pi))**(1/3)
 print("radius of the sphere is ")
 print('%0.3f'%r)
