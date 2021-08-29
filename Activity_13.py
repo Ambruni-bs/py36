f4e7b3af59e26db85a6de5a597423a1dbd26b5fb
Ambruni-bs <89661010+Ambruni-bs@users.noreply.github.com>
2021-08-29 19:46:58 +0530

    Update Activity_13.py

--- Activity_13.py
+++ Activity_13.py
@@ -4,3 +4,24 @@ n2 = int(input("Enter the end number: "))
 for num in range(n1, n2 + 1):
     if num % 2 != 0:
         print(num)
+        
+ #alternating methord
+
+def input_no():
+    a=int(input("enter the start number\n"))
+    b=int(input("enter the stop number\n"))
+    return a,b
+def oddnos(a,b):
+    res=[]
+    for num in range(a,b):
+        if(num%2!=0):
+            res.append(num)
+    return res
+def display(odd_nos):
+    print("odd nos from start to stop is:",odd_nos)
+
+def main():
+    a,b=input_no()
+    odd_nos=oddnos(a,b)
+    display(odd_nos)
+main()        
